ccb11f1bbcfcb3deab83d523ce657d4d14179fd7
lokesh <lokeech@gmail.com>
2019-08-04 20:05:28 +0530

    addedConsoleStepByLead2

--- Specs/SampleTest.js
+++ Specs/SampleTest.js
@@ -1,5 +1,6 @@
 describe('',function(){
     it('',function(){
         console.log("New test case created by Automation lead 1");
+        Console.log("New Test Step added By Automation Lead 2");
     })
 })
